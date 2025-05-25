19f4aa80bdb81dde682cb17ba094957748960d33
Erick Cordeiro <erickcordeiroa@gmail.com>
2025-05-25 13:20:26 -0300

    liberando cors geral

--- src/server.ts
+++ src/server.ts
@@ -8,13 +8,7 @@ dotenv.config();
 
 const app = express();
 
-
-const corsOptions = {
-  origin: "*",
-  optionsSuccessStatus: 200
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.json());
 app.use('/api/v1/', router);
